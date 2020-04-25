73cd3643c16ebfbada9f8d229c9151e50dc98a59
Amit <amit-kmr@live.com>
2020-04-25 18:01:23 +0530

    Ready for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const hbs = require('hbs')
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
@@ -84,7 +85,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () =>
+app.listen(port, () =>
 {
-    console.log('The server is up on port 3000.')  
+    console.log('The server is up on port ' + port + '.')
 })
